01d0455096cc701940260f025fdb2a463d4caaf3
Jason VanDeventer <jasonvandeventer@gmail.com>
2020-04-06 20:54:02 -0500

    Modified formatting

--- seaplane_gui.py
+++ seaplane_gui.py
@@ -9,11 +9,12 @@ import math
 # radius of Earth in kilometers
 R = 6383.0
 
+
 def calculate():
     first_text.delete(0, tk.END)
     first_city_text.delete(0, tk.END)
     second_city_text.delete(0, tk.END)
-    
+
     if first_drop.get() in cities:
         code1 = cities[first_drop.get()]
         name1 = airport_names[code1]
@@ -27,7 +28,7 @@ def calculate():
     if code2 in latitudes:
         second_lat = latitudes[code2]
     if code2 in longitudes:
-        second_lon = longitudes[code2] 
+        second_lon = longitudes[code2]
 
     lat1 = math.radians(float(first_lat))
     lon1 = math.radians(float(first_lon))
@@ -37,7 +38,8 @@ def calculate():
     dlon = lon2 - lon1
     dlat = lat2 - lat1
 
-    a = math.sin(dlat / 2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2)**2
+    a = math.sin(dlat / 2)**2 + math.cos(lat1) * \
+        math.cos(lat2) * math.sin(dlon / 2)**2
 
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
 
@@ -45,10 +47,11 @@ def calculate():
 
     # convert kilometers to nautical miles
     nmi = distance / 1.852
-    
+
     first_text.insert(tk.END, str(round(nmi, 2)))
     first_city_text.insert(tk.END, name1)
     second_city_text.insert(tk.END, name2)
+
 
 # Create window object
 app = tk.Tk()
@@ -56,8 +59,8 @@ app.title('Seaplane Calculator')
 app.geometry('500x250')
 app.iconbitmap('airplane.ico')
 
-#dictionaries
-cities = {'Atlanta': 'atl', 'Dallas': 'dfw', 'Tulsa': 'tul', 
+# dictionaries
+cities = {'Atlanta': 'atl', 'Dallas': 'dfw', 'Tulsa': 'tul',
           'New York': 'jfk', 'Los Angeles': 'lax', 'London': 'lhr',
           'Chicago': 'ord', 'Seattle': 'sea', 'Paris': 'cdg',
           'Shanghai': 'pvg', 'Hong Kong': 'hkg', 'Tokyo': 'hnd',
@@ -76,7 +79,7 @@ airport_names = {"atl": "Hartsfield-Jackson Atlanta International Airport",
                  "hnd": "Haneda Airport",
                  "dxb": "Dubai International Airport",
                  "pek": "Beijing Capital International Airport",
-                 "cgh": "Sao Paulo Congonhas Airport"}          
+                 "cgh": "Sao Paulo Congonhas Airport"}
 latitudes = {"atl": 33.6407, "dfw": 32.8998, "jfk": 40.6413,
              "lax": 33.9416, "lhr": 51.4700, "ord": 41.9742,
              "sea": 47.4502, "tul": 36.1988, "cdg": 49.0097,
@@ -90,7 +93,7 @@ longitudes = {"atl": -84.4277, "dfw": -97.0403, "jfk": -73.7781,
 
 # first airport
 first_airport_label = tk.Label(app, text='First City:',
-    font=('bold', 14), padx=10, pady=5)
+                               font=('bold', 14), padx=10, pady=5)
 first_airport_label.grid(column=0, row=5, sticky=tk.W)
 first_drop = ttk.Combobox(app, values=sorted(cities.keys()))
 pp.pprint(dict(first_drop))
@@ -99,19 +102,19 @@ first_drop.current(0)
 
 # airport name textboxes
 first_city_text_label = tk.Label(app, text='Name of airport:',
-    font=('bold', 14), padx=10, pady=5)
+                                 font=('bold', 14), padx=10, pady=5)
 first_city_text_label.grid(column=0, row=10, sticky=tk.W)
 first_city_text = ttk.Entry(app)
 first_city_text.grid(column=1, row=10, sticky=tk.EW, ipadx=65)
 second_city_text_label = tk.Label(app, text='Name of airport:',
-    font=('bold', 14), padx=10, pady=5)
+                                  font=('bold', 14), padx=10, pady=5)
 second_city_text_label.grid(column=0, row=20, sticky=tk.W)
 second_city_text = ttk.Entry(app)
 second_city_text.grid(column=1, row=20, columnspan=2, sticky=tk.EW)
 
 # second airport
 second_airport_label = tk.Label(app, text='Second City:',
-    font=('bold', 14), padx=10, pady=5)
+                                font=('bold', 14), padx=10, pady=5)
 second_airport_label.grid(column=0, row=15, sticky=tk.W)
 second_drop = ttk.Combobox(app, values=sorted(cities.keys()))
 pp.pprint(dict(second_drop))
@@ -119,7 +122,8 @@ second_drop.grid(column=1, row=15)
 second_drop.current(0)
 
 # create result textbox
-first_text_label = tk.Label(app, text="Distance in nautical miles:", font=('bold', 14))
+first_text_label = tk.Label(app, text="Distance in nautical miles:",
+                            font=('bold', 14))
 first_text_label.grid(column=0, row=25, padx=10, pady=5)
 first_text = ttk.Entry(app)
 first_text.grid(column=1, row=25, padx=10, pady=5)
